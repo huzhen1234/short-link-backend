90727092cf346ff268cb6e67b5460cead8034362
huzhen <2027280373@qq.com>
2025-07-19 23:19:37 +0800

    短链框架

---
+++ short-link-common/src/main/java/com/hutu/shortlinkcommon/ShortLinkCommonApplication.java
@@ -0,0 +1,13 @@
+package com.hutu.shortlinkcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkCommonApplication.class, args);
+    }
+
+}
