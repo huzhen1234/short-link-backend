845c9dd148331600b54d1f70f74dbd9f9265928c
huzhen <huzhen@bestpay.com.cn>
2025-08-04 16:22:06 +0800

    短链模块 mq 队列

--- short-link-link/src/main/java/com/hutu/shortlinklink/mq/consumer/StudentDlqListener.java
+++ short-link-link/src/main/java/com/hutu/shortlinklink/mq/consumer/StudentDlqListener.java
@@ -1,6 +1,7 @@
 package com.hutu.shortlinklink.mq.consumer;
 
 import com.hutu.shortlinkcommon.common.RocketMQConstant;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
@@ -11,10 +12,11 @@ import org.springframework.stereotype.Component;
     topic = "%DLQ%" + RocketMQConstant.TOPIC_SHORT_LINK_EVENT, // 死信队列名称必须严格匹配
     consumerGroup = "DLQ_HANDLER_GROUP" // 死信处理组（需不同组名）
 )
+@Slf4j
 public class StudentDlqListener implements RocketMQListener<MessageExt> {
     @Override
     public void onMessage(MessageExt message) {
-        System.err.println("[死信消息] 原因: " + message.getReconsumeTimes() + "次重试失败");
+       log.error("[死信消息] ~~~~~~");
         // 人工处理逻辑（如告警、补偿等）
     }
 }
